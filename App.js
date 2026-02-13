39c94e745c1e3ffe2dce7e9fa90001492ed775c4
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-02-12 17:45:32 -0800

    Refactor App component to integrate Redux store and implement bottom tab navigation for AnimalList and Favorites screens

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Provider, useSelector } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
+import store from './store';
+import AnimalListScreen from './screens/AnimalListScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
+import { setFavorites } from './features/animals/animalsSlice';
+
+const Tab = createBottomTabNavigator();
+
+/**
+ * RootNav is separated so we can use hooks (dispatch/select) after Provider.
+ */
+function RootNav() {
+  // NOTE: In Signpost 7 you will hydrate favorites here.
+  // TODO (Signpost 7): load favorites from AsyncStorage and dispatch(setFavorites(...))
+  // Hint:
+  // React.useEffect(() => {
+  //   const load = async () => {
+  //     const stored = await AsyncStorage.getItem('favorites');
+  //     if (stored) dispatch(setFavorites(JSON.parse(stored)));
+  //   };
+  //   load();
+  // }, []);
+
+  const favoritesCount = useSelector((state) => state.animals.favorites.length);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerTitleAlign: 'center',
+          tabBarIcon: ({ focused, color, size }) => {
+            const icons = {
+              Explore: focused ? 'paw' : 'paw-outline',
+              Favorites: focused ? 'heart' : 'heart-outline',
+            };
+            return <Ionicons name={icons[route.name]} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Explore" component={AnimalListScreen} />
+        <Tab.Screen
+          name="Favorites"
+          component={FavoritesScreen}
+          options={{
+            // Optional badge: show count when > 0
+            tabBarBadge: favoritesCount > 0 ? favoritesCount : undefined,
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <Provider store={store}>
+      <RootNav />
+    </Provider>
+  );
+}
